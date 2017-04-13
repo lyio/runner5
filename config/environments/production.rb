eeb63ba9d1fa45cd35fb18e561e73a29166f2324
Thomas Fiedler <thfied@gmail.com>
2017-04-13 23:37:55 +0200

    #5 Heroku setup + include pg and 12factor gems + Change production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -83,4 +83,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    # ActionMailer configuration
+ config.action_mailer.delivery_method = :smtp
+ config.action_mailer.smtp_settings = {
+   address:              'smtp.gmail.com',
+   port:                 587,
+   domain:               'runner5.de',
+   user_name:            ENV[EMAIL],
+   password:             ENV[EMAIL_PWD],
+   authentication:       'plain',
+   enable_starttls_auto: true
+ }
 end
