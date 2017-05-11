b6c756e31cf1e2e03253a65a8e2c7bbc0cce92ee
Thomas Fiedler <thfied@gmail.com>
2017-05-11 10:16:05 +0200

    #19 Update to Rails 5.1 + Run update and enable yarn/webpack

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
-source 'http://rubygems.org'
+source 'https://rubygems.org'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.0.0'
+gem 'rails', '~> 5.1.0'
 
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
@@ -15,10 +15,9 @@ gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
-# Font awesome for icons
-gem 'font-awesome-rails'
-
 gem 'bootstrap-sass'
+
+gem 'webpacker'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=172800'
+      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -50,17 +50,16 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # ActionMailer configuration
- config.action_mailer.delivery_method = :smtp
- config.action_mailer.smtp_settings = {
-   address:              'smtp.gmail.com',
-   port:                 587,
-   domain:               'runner5.de',
-   user_name:            'track.runner5.app@gmail.com',
-   password:             'BezmKYD408s753PL5SmQ',
-   authentication:       'plain',
-   enable_starttls_auto: true
- }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'runner5.de',
+    user_name:            'track.runner5.app@gmail.com',
+    password:             'BezmKYD408s753PL5SmQ',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
